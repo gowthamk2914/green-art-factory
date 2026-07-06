028fbb17f6adcbc1e376c9386e49e77e2c304ef1
gowthamk2914 <gowtham@mindmade.in>
2026-07-06 11:59:51 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,30 @@
 import "./globals.css";
 import Navbar from "../components/common/Navbar";
 import { ReactNode } from "react";
+import type { Metadata } from "next";
+import favicon from "@/public/images/gaf_favicon.png";
+
+import { Poppins } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: "Green Art Factory",
+  description: "Green Art Factory",
+  icons: {
+    icon: favicon.src,
+    shortcut: favicon.src,
+    apple: favicon.src,
+  },
+};
+
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  display: "swap",
+});
+
 
 export default function RootLayout({
   children,
@@ -9,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <Navbar />
         {children}
       </body>
